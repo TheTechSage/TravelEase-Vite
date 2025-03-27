62fd71c3f5891a0acef2ab9080a093e2af270e3c
priti <pritiroy.manoj@gmail.com>
2025-03-27 14:36:50 +0530

    basic structure of Footer component

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,10 +3,91 @@ import React from 'react'
 function Footer() {
     return (
         <>
-            <div>
-                <h1>Footer</h1>
-            </div>
-        </>
+        <footer class=" bg-black pt-8  w-full box-border">
+        <div class="container flex">
+            <div class="flex flex-col text-white pl-25">
+    <img class="h-10" src="logo-light-q8SM9mVG.svg"></img>
+    <p>Departure defective arranging rapturous did believe him all had supported.</p>
+    <p>+1234 568 963</p>
+    <p>example@gmail.com</p>
+    </div>
+        <div class="pl-30 ">
+        <h5 class="text-2xl text-white mb-3">Page</h5>
+        <div class="flex flex-col" >
+        <ul>
+            <li className="text-white hover:text-blue-500"><a href="#">About us</a></li>
+            <li className="text-white hover:text-blue-500"><a href="#">Contact us</a></li>
+            <li className="text-white hover:text-blue-500"><a href="#">News and blog</a></li>
+            <li className="text-white hover:text-blue-500"><a href="#">Meet a team</a></li>
+        </ul>
+    </div>
+    </div>
+        <div class="pl-30">
+            <h5 class=" text-white text-2xl mb-3">Link</h5>
+            <div className="flex flex-col ">
+            <ul>
+                <li className="text-white hover:text-blue-500"><a href="#">Signout</a></li>
+                <li className="text-white hover:text-blue-500"><a href="#">Signin</a></li>
+                <li className="text-white hover:text-blue-500"><a href="#">Privacy Policy</a></li>
+                <li className="text-white hover:text-blue-500"><a href="#">Terms</a></li>
+                <li className="text-white hover:text-blue-500"><a href="#">Cookies</a></li>
+                <li className="text-white hover:text-blue-500"><a href="#">Support</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="pl-30">
+     <h4 class="text-2xl text-white mb-3">Globel Site</h4>
+     <div class="flex flex-col">
+        <ul class="text-white">
+            <li><a href="#india">India</a></li>
+            <li><a href="#California">California</a></li>
+            <li><a href="#indonasia">Indonasia</a></li>
+            <li><a href="#canada">Canada</a></li>
+            <li><a href="#Malaysia">Malaysia</a></li>
+            </ul>
+            </div>   
+    </div>
+<div class="pl-30 pr-50">
+    <h3 class="text-2xl text-white mb-3">Booking</h3>
+    <div class="flex flex-col">
+        <ul class="text-white "> 
+            <li><a href="#hotel">Hotel</a></li>
+            <li><a href="#flight">Flight</a></li>
+            <li><a href="#tour">Tour</a></li>
+            <li><a href="#cabs">Cabs</a></li>
+        </ul>
+    </div>
+    </div>
+    
+</div>
+<div class="section">
+        <h5 class="text-2xl text-white">Top Links</h5>
+        <div class="flex flex-wrap gap-4 text-white px-6">
+            <div><a href="#flight">Flight</a></div>
+            <div><a href="#">Hotels</a></div>
+            <div> <a href="#">Tour</a></div>
+            <div><a href="#">Cabs</a></div>
+            <div>About</div>
+            <div>Contact Us</div>
+            <div>Blogs</div>
+            <div>Services</div>
+            <div>Detail page</div>
+            <div>Policy</div>
+            <div>Testimonials</div>
+            <div>Newsletters</div>
+            <div>Podcast</div>
+            <div>Protest</div>
+            <div>NewsCybers</div>
+            <div>Educations</div>
+            <div>Sports</div>
+            <div><a href="#">Tech And Auto</a></div>
+            <div><a href="#">Opinion</a></div>
+            <div><a href="#">Share Market</a></div>
+        </div>
+    </div>
+
+ </footer>
+         </>
     )
 }
 
