5ab800cfedc0daa6bc7bc1c73afdb64acb86c540
priya <priyaroy.sanoj@gmail.com>
2025-03-27 14:38:30 +0530

    basic structure of header component

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,9 +3,21 @@ import React from 'react'
 function Header() {
     return (
         <>
+        <div className='flex gap-5'>
             <div>
-                <h1>Header</h1>
+            <img class="h-15 pl-15 pt-8" src="logo-XeOrDTLq.svg"></img>
+            </div> 
+            <div class="flex gap-10 pt-8">
+             <a href="#listings">Listings</a>  
+               <a href="#pages">Pages</a>
+                <a href="#account">Accounts</a> 
             </div>
+            <div class="flex gap-10 pt-8">
+             <a href="#hotel">Hotel</a>  
+               <a href="#flight">Flight</a>
+                <a href="#tour">Tour</a> 
+            </div>
+        </div>
         </>
     )
 }
