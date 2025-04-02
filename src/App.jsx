90fa50a9e0a489e773132d35cb9a6c8f5cc42ec0
priti <pritiroy.manoj@gmail.com>
2025-04-02 11:25:15 +0530

    add some footer components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/common/Header'
 import Footer from './components/common/Footer'
+import { IoLocationOutline } from "react-icons/io5";
+import { CiCalendar,CiSearch } from "react-icons/ci";
+import { FaRegUser } from "react-icons/fa6";
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +15,39 @@ function App() {
   return (
     <>
       <Header />
-        
+      <div class="flex ">
+        <div class="flex-col">
+    <h1 class="text-6xl px-20 py-5">Find the top</h1>
+    <span class="text-6xl px-20">Hotel  nearby </span>
+    <p class="p-10 opacity-75 text-xl">We bring you not only a stay option, but an experience in your budget to enjoy the luxury.</p>
+    <div class="flex p-5 gap-5">
+<button class="bg-blue-300 text-blue-800 bg-opacity-75 hover:bg-blue-800 hover:text-white px-6 py-3 " > Discover Now </button>
+      <p>Watch Our Story</p>
+    </div>
+    <div>
+    <form class="flex border-3 border-solid  rounded-xl">
+    <IoLocationOutline size="40" /><input type="location" placeholder='Select Location'></input>
+    <CiCalendar  size="40"/><input type="date-time-local"placeholder="Current time"></input>
+    <FaRegUser  size="40"/><input type="text" placeholder=""></input>
+    <CiSearch color="blue" size="40" />
+    </form>
+    </div>
+    </div>
+    <div class="p-4">
+      <img  class="w-xl rounded-xl" src="06-hDeq7Yvr.jpg"></img>
+    
+      <div class=" flex absolute top-20 left-193">
+      <img class=" h-10 rounded-full "src="11-gZgLx6ik.jpg"></img>
+      </div>
+<div class="absolute top-45 left-190">
+      <img class=" h-15 rounded-full "src="11-gZgLx6ik.jpg"></img>
+      </div>
+    </div>
+    
+    </div>
+    
+  
+
       <Footer />
     </>
   )
--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,90 +1,129 @@
 import React from 'react'
-
+import { MdOutlinePhone,MdOutlineMailOutline,MdFlight } from "react-icons/md";
+import { FaFacebookSquare ,FaInstagram ,FaCar,FaGlobeAsia, FaHotel,FaTwitter ,FaLinkedin } from "react-icons/fa";
 function Footer() {
     return (
         <>
-        <footer class=" bg-black pt-8  w-full box-border">
-        <div class="container flex">
-            <div class="flex flex-col text-white pl-25">
-    <img class="h-10" src="logo-light-q8SM9mVG.svg"></img>
-    <p>Departure defective arranging rapturous did believe him all had supported.</p>
-    <p>+1234 568 963</p>
-    <p>example@gmail.com</p>
+        <footer class=" bg-black pt-8 ">
+        <div class="container flex gap-20 pl-15 pr-15">
+            <div class="flex flex-col text-white">
+                <div class="pl-2">
+    <img class="h-10 " src="logo-light-q8SM9mVG.svg"></img>
     </div>
-        <div class="pl-30 ">
+    <div class="opacity-75  ">
+    <p>Departure defective arranging rapturous did believe him all had supported.</p>
+</div>  
+<div class="p-2">
+    <a  class="text-white opacity-75 hover:text-blue-500 gap-2 flex"href="#"> <MdOutlinePhone size="25px"/> +1234 567 678</a>
+    <a class="=text-white opacity-75 hover:text-blue-500  gap-2 flex"href="#"><MdOutlineMailOutline  size="25px"/>example@gmail.com</a>
+    </div>
+</div>
+        <div>
         <h5 class="text-2xl text-white mb-3">Page</h5>
         <div class="flex flex-col" >
-        <ul>
-            <li className="text-white hover:text-blue-500"><a href="#">About us</a></li>
-            <li className="text-white hover:text-blue-500"><a href="#">Contact us</a></li>
-            <li className="text-white hover:text-blue-500"><a href="#">News and blog</a></li>
-            <li className="text-white hover:text-blue-500"><a href="#">Meet a team</a></li>
+        <ul class="gap-5 opacity-75">
+            <li className="text-white hover:text-blue-500 pb-2"><a href="#">About us</a></li>
+            <li className="text-white hover:text-blue-500 pb-2"><a href="#">Contact us</a></li>
+            <li className="text-white  hover:text-blue-500 pb-2"><a href="#">News and blog</a></li>
+            <li className="text-white  hover:text-blue-500 pb-2"><a href="#">Meet a team</a></li>
         </ul>
     </div>
     </div>
-        <div class="pl-30">
-            <h5 class=" text-white text-2xl mb-3">Link</h5>
+        <div>
+            <h5 class=" text-white  text-2xl mb-3">Link</h5>
             <div className="flex flex-col ">
-            <ul>
-                <li className="text-white hover:text-blue-500"><a href="#">Signout</a></li>
-                <li className="text-white hover:text-blue-500"><a href="#">Signin</a></li>
-                <li className="text-white hover:text-blue-500"><a href="#">Privacy Policy</a></li>
-                <li className="text-white hover:text-blue-500"><a href="#">Terms</a></li>
-                <li className="text-white hover:text-blue-500"><a href="#">Cookies</a></li>
-                <li className="text-white hover:text-blue-500"><a href="#">Support</a></li>
+            <ul class="opacity-75">
+                <li className="text-white  hover:text-blue-500 pb-2"><a href="#">Signout</a></li>
+                <li className="text-white hover:text-blue-500 pb-2"><a href="#">Signin</a></li>
+                <li className="text-white  hover:text-blue-500 pb-2"><a href="#">Privacy Policy</a></li>
+                <li className="text-white  hover:text-blue-500 pb-2"><a href="#">Terms</a></li>
+                <li className="text-white  hover:text-blue-500 pb-2"><a href="#">Cookies</a></li>
+                <li className="text-white   hover:text-blue-500 pb-2"><a href="#">Support</a></li>
             </ul>
         </div>
     </div>
-    <div class="pl-30">
-     <h4 class="text-2xl text-white mb-3">Globel Site</h4>
+    <div>
+     <h4 class="text-2xl text-white pb-3 ">Globel Site</h4>
      <div class="flex flex-col">
-        <ul class="text-white">
-            <li><a href="#india">India</a></li>
-            <li><a href="#California">California</a></li>
-            <li><a href="#indonasia">Indonasia</a></li>
-            <li><a href="#canada">Canada</a></li>
-            <li><a href="#Malaysia">Malaysia</a></li>
+        <ul class="opacity-75">
+            <li className="text-white opacity-75 hover:text-blue-500 pb-2"><a href="#">India</a></li>
+            <li className="text-white opacity-75 hover:text-blue-500 pb-2"><a href="#California">California</a></li>
+            <li className="text-white hover:text-blue-500 opacity-75 pb-2"><a href="#indonasia">Indonasia</a></li>
+            <li className="text-white hover:text-blue-500 opacity-75 pb-2"><a href="#canada">Canada</a></li>
+            <li className="text-white hover:text-blue-500 opacity-75 pb-2"><a href="#Malaysia">Malaysia</a></li>
             </ul>
             </div>   
     </div>
-<div class="pl-30 pr-50">
-    <h3 class="text-2xl text-white mb-3">Booking</h3>
+<div>
+    <h3 class="text-2xl text-white pb-3">Booking</h3>
     <div class="flex flex-col">
-        <ul class="text-white "> 
-            <li><a href="#hotel">Hotel</a></li>
-            <li><a href="#flight">Flight</a></li>
-            <li><a href="#tour">Tour</a></li>
-            <li><a href="#cabs">Cabs</a></li>
+        <ul class="opacity-75 "> 
+            <li className="text-white hover:text-blue-500 pb-2 gap-2 flex"><FaHotel size="20px" /><a href="#hotel">Hotel</a></li>
+            <li className="text-white hover:text-blue-500 pb-2 gap-2 flex"><MdFlight size="20px" /><a href="#flight">Flight</a></li>
+            <li className="text-white hover:text-blue-500 pb-2 gap-2 flex"><FaGlobeAsia  size="20px"/><a href="#tour">Tour</a></li>
+            <li className="text-white hover:text-blue-500 pb-2  gap-2 flex"> <FaCar  size="20px"/><a href="#cabs">Cabs</a></li>
         </ul>
     </div>
     </div>
     
 </div>
-<div class="section">
-        <h5 class="text-2xl text-white">Top Links</h5>
-        <div class="flex flex-wrap gap-4 text-white px-6">
-            <div><a href="#flight">Flight</a></div>
-            <div><a href="#">Hotels</a></div>
-            <div> <a href="#">Tour</a></div>
-            <div><a href="#">Cabs</a></div>
-            <div>About</div>
-            <div>Contact Us</div>
-            <div>Blogs</div>
-            <div>Services</div>
-            <div>Detail page</div>
-            <div>Policy</div>
-            <div>Testimonials</div>
-            <div>Newsletters</div>
-            <div>Podcast</div>
-            <div>Protest</div>
-            <div>NewsCybers</div>
-            <div>Educations</div>
-            <div>Sports</div>
-            <div><a href="#">Tech And Auto</a></div>
-            <div><a href="#">Opinion</a></div>
-            <div><a href="#">Share Market</a></div>
+<div class="section py-15 px-15 ">
+        <h5 class="text-2xl text-white pb-2">Top Links</h5>
+        <div class="flex flex-wrap gap-5 pr-15 opacity-75">
+            <div className="text-white hover:text-blue-500 "><a href="#flight">Flight</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Hotels</a></div>
+            <div className="text-white hover:text-blue-500"> <a href="#">Tour</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Cabs</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">About</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Contact Us</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Blogs</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Services</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Detail page</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Policy</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Testimonials</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Newsletters</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Podcast</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Protest</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">NewsCybers</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Educations</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Sports</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Tech And Auto</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Opinion</a></div>
+            <div className="text-white hover:text-blue-500"><a href="#">Share Market</a></div>
         </div>
     </div>
+    <div class="flex gap-200 p-10">
+        <div>
+            <h3 class="text-2xl text-white">Payment & Security</h3>
+            <div class="flex gap-2 py-3">
+            <img class="h-8" src="paypal-p0Cf0jrR.svg"></img>
+            <img  class="h-8"src="download.svg"></img>
+            <img  class="h-8"src="mastercard-7AdMRYoz.svg"></img>
+            <img class="h-8" src="expresscard-lzb9gcWA.svg"></img>
+        </div>
+        </div>
+        <div>
+            <h3 class="text-2xl text-white">Follow us on</h3>
+            <div class="flex gap-3">
+         <a  href="https://facebook.com"> <FaFacebookSquare color="blue" size="30"/></a>
+           <a href="https://instagram.com"><FaInstagram size="30"  color=" pink"/>
+           </a> 
+           <a href="https://twitter.com"><FaTwitter color="blue" size="30" /></a>
+           <a href="https://linkedin.com"><FaLinkedin  color="blue" size="30"/></a>
+           </div>
+        </div>
+         </div>
+         <div class="flex gap-100 ">
+            <div class="pl-5">
+                <h4 class=" text-white opacity-75">Copyrights ©2025 Booking. Build by StackBros</h4>
+    </div>
+            <div class="right flex gap-20 opacity-75 ">
+                <h2 className="text-white hover:text-blue-500"><a href="#">Privacy policy</a></h2>
+                <h2 className="text-white hover:text-blue-500"><a href="#">Terms and Conditions</a></h2>
+                <h2 className="text-white hover:text-blue-500"><a href="#">Refund policy</a></h2>
+            </div>
+         </div>
+         
 
  </footer>
          </>
