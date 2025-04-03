baca363aa58afb8184b48122c021c6311f09edf9
priti <pritiroy.manoj@gmail.com>
2025-04-03 12:29:56 +0530

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/common/Header'
 import Footer from './components/common/Footer'
-//import Hero from './hero/Hero'
+import Hero from './components/common/hero/Hero'
 import { IoLocationOutline } from "react-icons/io5";
 import { CiCalendar,CiSearch } from "react-icons/ci";
 import { FaRegUser } from "react-icons/fa6";
@@ -14,7 +14,7 @@ import { ImSpoonKnife } from "react-icons/im";
  import { LuClock11 } from "react-icons/lu";
 import { MdOutlineSecurity,MdOutlineElectricBolt } from "react-icons/md";
 import { ImStarFull } from "react-icons/im";
- import { FaStarHalfAlt } from "react-icons/fa";
+ import { FaStarHalfAlt ,FaHandHoldingHeart, FaHandHoldingUsd } from "react-icons/fa";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -120,38 +120,95 @@ function App() {
 <div class="manyimage ">
   <div class="px-20 py-20">
     <h2 class="text-5xl font-bold text-center">Explore Nearby</h2>
-    <div class=" flex flex-wrap gap-5 py-10">
-    <a  class="hover:text-blue-500 text-xl px-5"href="#">
+
+    <div class=" flex flex-wrap gap-10 py-10">
+      <div>
+    <a  class="hover:text-blue-500 text-xl text-center px-5"href="#">
     <img  class="rounded-full w-35"src="01-SppmAcgj (1).jpg"></img> San Francisco </a>
-    <a  class="hover:text-blue-500 text-xl px-5"href="#">
+    <p class="opacity-75 text-center">13 mins drive</p>
+    </div>
+    <div>
+    <a  class="hover:text-blue-500 text-xl  text-center px-5"href="#">
     <img  class="rounded-full w-35"src="02-GRL65llU.jpg"></img> Los Angeles </a>
+        <p class="opacity-75 text-center">25 mins drive</p>
+    </div>
+    <div>
     <a  class="hover:text-blue-500 text-xl px-5"href="#">
     <img  class="rounded-full w-35"src="03-4mB89xkp.jpg"></img> Miami </a>
+        <p class="opacity-75  text-center">45 mins drive</p>
+    </div>
+    <div>
     <a  class="hover:text-blue-500 text-xl px-5"href="#">
     <img  class="rounded-full w-35"src="04-pnduE6qY.jpg"></img> Sanjosh </a>
+        <p class="opacity-75  text-center">55 mins drive</p>
+    </div>
+    <div>
     <a  class="hover:text-blue-500 text-xl px-5"href="#">
     <img  class="rounded-full w-35"src="05-2KGCHYz-.jpg"></img> New york </a>
+        <p class="opacity-75  text-center">1 hour drive</p>
+    </div>
+    <div>
     <a  class="hover:text-blue-500 text-xl px-5"href="#">
     <img  class="rounded-full w-35"src="06-NVeYwMw_.jpg"></img> North Justen </a>
+        <p class="opacity-75  text-center">2 hour drive</p>
+    </div>
+    <div>
     <a  class="hover:text-blue-500 text-xl px-5"href="#">
     <img  class="rounded-full w-35"src="07-cKl6NSY7.jpg"></img> Rio </a>
+        <p class="opacity-75  text-center">20 mins drive</p>
+    </div>
+    <div>
     <a  class="hover:text-blue-500 text-xl px-5 "href="#">
     <img  class="rounded-full w-35"src="08-mIVX0-EV.jpg"></img> Las Vegas </a>
+     <p class="opacity-75  text-center"> 3 hours drive</p>
+    </div>
+    <div>
     <a  class="hover:text-blue-500 text-xl px-5"href="#">
     <img  class="rounded-full w-35"src="09-U3qhstmi.jpg"></img> Texas</a>
+     <p class="opacity-75  text-center"> 55 mins drive</p>
+    </div>
+    <div>
     <a  class="hover:text-blue-500 text-xl px-5"href="#">
     <img  class="rounded-full w-35"src="10-wqfqgIeR.jpg"></img> Chicago </a>
+     <p class="opacity-75  text-center">  13 mins drive</p>
+    </div>
+    <div>
     <a  class="hover:text-blue-500 text-xl px-5"href="#">
-    <img  class="rounded-full w-35"src="11-GaBY5L70.jpg"></img>New Keagan </a>
+    <img  class="rounded-full w-35"src="11-GaBY5L70.jpg"></img><p class="text-center">New Keagan</p> </a>
+     <p class="opacity-75  text-center">  35 mins drive</p>
+    </div>
+    <div>
     <a  class="hover:text-blue-500 text-xl px-5"href="#">
     <img  class="rounded-full w-35"src="01-SppmAcgj (1).jpg"></img> Osla</a>
+        <p class="opacity-75  text-center"> 1 hour 13 mins drive</p>
+    </div>
 </div>
   </div>
 </div>
- 
+<div class="container">
+  <div class="flex px-20 py-10">
+  <FaHandHoldingHeart size="50"/>
+  <div class="flex-col ">
+  <h2 class="font-bold text-xl p-2">24x7 Help</h2>
+ <p class="opacity-75 p-2"> If we fall short of your expectation in any way, let us know</p>
+ </div>
 
+ <FaHandHoldingUsd  size="50"/>
+ <div class="flex-col ">
+  <h2 class="font-bold text-xl p-2">Payment Trust</h2>
+ <p class="opacity-75 p-2"> All refunds come with no questions asked guarantee</p>
+ </div>
+</div>
+</div>
+ <div class="pl-20  pb-5">
+  <h2 class="font-bold text-2xl">Downloaded app</h2>
+  <div class="flex gap-8 pt-5 ">
+  <a href="#"><img class="h-18" src="google-play-Oy7Jtnye.svg"></img></a>
+  <a href="#"><img  class="h-18"src="app-store-n2CVq0lh.svg"></img></a>
+ </div>
+</div>
      
-  
+  <Hero/>
 
       <Footer />
     </>
