8c880fdde581fcf65867c2caa6f91d9b71168242
priya <priyaroy.sanoj@gmail.com>
2025-04-07 18:10:54 +0530

    modify components Takeoff

--- src/components/flight/Takeoff.jsx
+++ src/components/flight/Takeoff.jsx
@@ -10,37 +10,14 @@ function Takeoff () {
     return (
         <> 
         <div className="flex">
-    <nav className="w-full h-16 bg-blue-500 items-center text-white">
+    <nav className="w-full h-10 bg-blue-500 items-center text-white">
             <h4>Traveling internationally? 
             Get update information on COVID-19 travel guidance and restrictions</h4>
-            <div>
-                <button>Learn more!</button>
-            </div>
-            <div>
-               <RxCross2 />
+            <div className="justify-items-end pt-0">
+               <RxCross2 size={20} />
             </div>
         </nav>
-        </div>
-
-        <div>
-        <img className="h-18 pl-18 pt-8" src="logo-XeOrDTLq.svg"></img>
-        </div>
-            
-          <div className="flex pt-10 pl-12 gap-8 font-arial">
-        <div className="border-none cursor-pointer relative group hover:bg-sky-50">
-          <a href="#travels">
-         <ul className="flex items-center"><MdCardTravel />Travelers<IoIosArrowDown size={15}/></ul>
-            </a>
-            <div className="list-none border-none bg-white p-6 text-bold 
-             shadow-lg w-60 absolute hidden group-hover:flex flex-col">
-                <li className="list-none"> <FaHotel />Hotel</li>
-                  <li> <MdFlight />Flight</li>
-                  <li><FaGlobeAmericas />Tour</li>
-                  <li><FaCar />cab</li>
-              </div>
-             </div>
             </div>
-     
 
         </>
     )
