cbf30689eda08723a1cedb2ce82038949e51afd0
priti <pritiroy.manoj@gmail.com>
2025-04-07 16:54:52 +0530

    added explore nearby section

--- src/components/hero/Explore.jsx
+++ src/components/hero/Explore.jsx
@@ -1,100 +1,105 @@
 import React from 'react'
-import { FaStarHalfAlt ,FaHandHoldingHeart, FaHandHoldingUsd } from "react-icons/fa";
+import { FaHandHoldingHeart, FaHandHoldingUsd } from "react-icons/fa";
 function Explore() {
     return (
         <>
-       < div class="manyimage ">
-  <div class="px-20 py-20">
-    <h2 class="text-5xl font-bold text-center">Explore Nearby</h2>
+            < div className="manyimage ">
+                <div className="px-20 py-20">
+                    <h2 className="text-5xl font-bold text-center">Explore Nearby</h2>
 
-    <div class=" flex flex-wrap gap-10 py-10">
-      <div>
-    <a  class="hover:text-blue-500 text-xl text-center px-5"href="#">
-    <img  class="rounded-full w-35"src="01-SppmAcgj (1).jpg"></img> San Francisco </a>
-    <p class="opacity-75 text-center">13 mins drive</p>
-    </div>
-    <div>
-    <a  class="hover:text-blue-500 text-xl  text-center px-5"href="#">
-    <img  class="rounded-full w-35"src="02-GRL65llU.jpg"></img> Los Angeles </a>
-        <p class="opacity-75 text-center">25 mins drive</p>
-    </div>
-    <div>
-    <a  class="hover:text-blue-500 text-xl px-5"href="#">
-    <img  class="rounded-full w-35"src="03-4mB89xkp.jpg"></img> Miami </a>
-        <p class="opacity-75  text-center">45 mins drive</p>
-    </div>
-    <div>
-    <a  class="hover:text-blue-500 text-xl px-5"href="#">
-    <img  class="rounded-full w-35"src="04-pnduE6qY.jpg"></img> Sanjosh </a>
-        <p class="opacity-75  text-center">55 mins drive</p>
-    </div>
-    <div>
-    <a  class="hover:text-blue-500 text-xl px-5"href="#">
-    <img  class="rounded-full w-35"src="05-2KGCHYz-.jpg"></img> New york </a>
-        <p class="opacity-75  text-center">1 hour drive</p>
-    </div>
-    <div>
-    <a  class="hover:text-blue-500 text-xl px-5"href="#">
-    <img  class="rounded-full w-35"src="06-NVeYwMw_.jpg"></img> North Justen </a>
-        <p class="opacity-75  text-center">2 hour drive</p>
-    </div>
-    <div>
-    <a  class="hover:text-blue-500 text-xl px-5"href="#">
-    <img  class="rounded-full w-35"src="07-cKl6NSY7.jpg"></img> Rio </a>
-        <p class="opacity-75  text-center">20 mins drive</p>
-    </div>
-    <div>
-    <a  class="hover:text-blue-500 text-xl px-5 "href="#">
-    <img  class="rounded-full w-35"src="08-mIVX0-EV.jpg"></img> Las Vegas </a>
-     <p class="opacity-75  text-center"> 3 hours drive</p>
-    </div>
-    <div>
-    <a  class="hover:text-blue-500 text-xl px-5"href="#">
-    <img  class="rounded-full w-35"src="09-U3qhstmi.jpg"></img> Texas</a>
-     <p class="opacity-75  text-center"> 55 mins drive</p>
-    </div>
-    <div>
-    <a  class="hover:text-blue-500 text-xl px-5"href="#">
-    <img  class="rounded-full w-35"src="10-wqfqgIeR.jpg"></img> Chicago </a>
-     <p class="opacity-75  text-center">  13 mins drive</p>
-    </div>
-    <div>
-    <a  class="hover:text-blue-500 text-xl px-5"href="#">
-    <img  class="rounded-full w-35"src="11-GaBY5L70.jpg"></img><p class="text-center">New Keagan</p> </a>
-     <p class="opacity-75  text-center">  35 mins drive</p>
-    </div>
-    <div>
-    <a  class="hover:text-blue-500 text-xl px-5"href="#">
-    <img  class="rounded-full w-35"src="01-SppmAcgj (1).jpg"></img> Osla</a>
-        <p class="opacity-75  text-center"> 1 hour 13 mins drive</p>
-    </div>
-</div>
-  </div>
-</div>
-<div class="container">
-  <div class="flex px-20 py-10">
-  <FaHandHoldingHeart size="50"/>
-  <div class="flex-col ">
-  <h2 class="font-bold text-xl p-2">24x7 Help</h2>
- <p class="opacity-75 p-2"> If we fall short of your expectation in any way, let us know</p>
- </div>
+                    <div className=" flex flex-wrap gap-5 py-15">
+                        <div className='flex pb-10 '>
+                        <div className='flex flex-col justify-center items-center'>
+                            <a className="hover:text-blue-500 text-xl text-center px-5" href="#">
+                                <img className="rounded-full w-35" src="01-SppmAcgj (1).jpg"></img> San Francisco </a>
+                            <p className="opacity-75 text-center">13 mins drive</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center'>
+                            <a className="hover:text-blue-500 text-xl  text-center px-5" href="#">
+                                <img className="rounded-full w-35" src="02-GRL65llU.jpg"></img> Los Angeles </a>
+                            <p className="opacity-75 text-center">25 mins drive</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center'>
+                            <a className="hover:text-blue-500 text-xl text-center px-5" href="#">
+                                <img className="rounded-full w-35" src="03-4mB89xkp.jpg"></img> Miami </a>
+                            <p className="opacity-75  text-center">45 mins drive</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center'>
+                            <a className="hover:text-blue-500 text-xl text-center px-5" href="#">
+                                <img className="rounded-full w-35" src="04-pnduE6qY.jpg"></img> Sanjosh </a>
+                            <p className="opacity-75  text-center">55 mins drive</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center'>
+                            <a className="hover:text-blue-500 text-xl text-center px-5" href="#">
+                                <img className="rounded-full w-35" src="05-2KGCHYz-.jpg"></img> New york </a>
+                            <p className="opacity-75  text-center">1 hour drive</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center'>
+                            <a className="hover:text-blue-500 text-xl text-center px-5" href="#">
+                                <img className="rounded-full w-35" src="06-NVeYwMw_.jpg"></img> North Justen </a>
+                            <p className="opacity-75  text-center">2 hour drive</p>
+                        </div>
+                        </div>
+                        <div className="flex pt-10">
+                        <div className='flex flex-col justify-center items-center'>
+                            <a className="hover:text-blue-500 text-xl text-center px-5" href="#">
+                                <img className="rounded-full w-35" src="07-cKl6NSY7.jpg"></img> Rio </a>
+                            <p className="opacity-75  text-center">20 mins drive</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center'>
+                            <a className="hover:text-blue-500 text-xl text-center px-5 " href="#">
+                                <img className="rounded-full w-35" src="08-mIVX0-EV.jpg"></img> Las Vegas </a>
+                            <p className="opacity-75  text-center"> 3 hours drive</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center'>
+                            <a className="hover:text-blue-500 text-xl text-center px-5" href="#">
+                                <img className="rounded-full w-35" src="09-U3qhstmi.jpg"></img> Texas</a>
+                            <p className="opacity-75  text-center"> 55 mins drive</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center'>
+                            <a className="hover:text-blue-500 text-xl text-center px-5" href="#">
+                                <img className="rounded-full w-35" src="10-wqfqgIeR.jpg"></img> Chicago </a>
+                            <p className="opacity-75  text-center">  13 mins drive</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center'>
+                            <a className="hover:text-blue-500 text-xl text-center px-5" href="#">
+                                <img className="rounded-full w-35" src="11-GaBY5L70.jpg"></img>New Keagan </a>
+                            <p className="opacity-75  text-center">  35 mins drive</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center'>
+                            <a className="hover:text-blue-500 text-xl text-center px-5" href="#">
+                                <img className="rounded-full w-35" src="01-SppmAcgj (1).jpg"></img> Oslo</a>
+                            <p className="opacity-75  text-center"> 1 hour 13 mins drive</p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container flex gap-10">
+                <div className=" pl-10 shadow border-3 border-white rounded-xl ">
+                    <FaHandHoldingHeart size="50" />
+                    <div className="flex-col ">
+                        <h2 className="font-bold text-xl p-2">24x7 Help</h2>
+                        <p className="opacity-75 p-2"> If we fall short of your expectation in any way, let us know</p>
+                    </div>
+                </div>
+                <div className="p-5 shadow border-3 border-white rounded-xl ">
+                    <FaHandHoldingUsd size="50" />
+                    <div className="flex-col ">
+                        <h2 className="font-bold text-xl p-2">Payment Trust</h2>
+                        <p className="opacity-75 p-2"> All refunds come with no questions asked guarantee</p>
+                    </div>
+                </div>
+            </div>
+            <div className="pl-20 pb-5">
+                <h2 className="font-bold text-2xl">Downloaded app</h2>
+                <div className="flex gap-8 pt-5  ">
+                    <a href="#"><img className="h-18" src="google-play-Oy7Jtnye.svg"></img></a>
+                    <a href="#"><img className="h-18" src="app-store-n2CVq0lh.svg"></img></a>
+                </div>
+            </div>
+        </>
+    )
+}
 
- <FaHandHoldingUsd  size="50"/>
- <div class="flex-col ">
-  <h2 class="font-bold text-xl p-2">Payment Trust</h2>
- <p class="opacity-75 p-2"> All refunds come with no questions asked guarantee</p>
- </div>
-</div>
-</div>
- <div class="pl-20  pb-5">
-  <h2 class="font-bold text-2xl">Downloaded app</h2>
-  <div class="flex gap-8 pt-5 ">
-  <a href="#"><img class="h-18" src="google-play-Oy7Jtnye.svg"></img></a>
-  <a href="#"><img  class="h-18"src="app-store-n2CVq0lh.svg"></img></a>
- </div>
-</div>     
-    </>
-     )
-    }
-    
-    export default Explore
+export default Explore
--- src/components/hero/FeaturedHotels.jsx
+++ src/components/hero/FeaturedHotels.jsx
@@ -1,41 +1,42 @@
 import React from 'react'
 import { ImStarFull } from "react-icons/im";
- import { FaStarHalfAlt ,FaHandHoldingHeart, FaHandHoldingUsd } from "react-icons/fa";
+ import { FaStarHalfAlt  } from "react-icons/fa";
  function FeaturedHotels() {
     return (
         <>
-        <div class="container">
-          <div class="p-10">
-            <h1 class="font-bold text-center text-5xl">Featured Hotels</h1>
+        <div className="container">
+          <div className="p-10">
+            <h1 className="font-bold text-center text-5xl">Featured Hotels</h1>
           </div>
-          <div class="flex px-50 py-10 gap-4 ">
-            <img class="w-60 rounded-2xl transition-transform duration-500 ease-in-out transform hover:scale-105" src="01-kkM7mXf9.jpg"></img>
-            <img class="w-60 rounded-2xl transition-transform duration-500 ease-in-out transform hover:scale-105" src="02-E_sTtfo3.jpg"></img>
-            <img  class="w-60 rounded-2xl transition-transform duration-500 ease-in-out transform hover:scale-105"src="03-B8eH2v8V.jpg"></img>
-            <img class="w-60 rounded-2xl transition-transform duration-500 ease-in-out transform hover:scale-105"src="04-SDLCq6RC.jpg"></img>
+          <div className="flex px-50 py-10 gap-4 ">
+            <img className="w-60 rounded-2xl transition-transform duration-500 ease-in-out transform hover:scale-105" src="01-kkM7mXf9.jpg"></img>
+            <img className="w-60 rounded-2xl transition-transform duration-500 ease-in-out transform hover:scale-105" src="02-E_sTtfo3.jpg"></img>
+            <img  className="w-60 rounded-2xl transition-transform duration-500 ease-in-out transform hover:scale-105"src="03-B8eH2v8V.jpg"></img>
+            <img className="w-60 rounded-2xl transition-transform duration-500 ease-in-out transform hover:scale-105"src="04-SDLCq6RC.jpg"></img>
           </div>
-        <div class="flex p-20 gap-20">
-    <img class="h-10 transition duration-300 ease-in-out hover:grayscale-0 grayscale" src="download (1).svg"></img>
-    <img class="h-10 transition duration-300 ease-in-out hover:grayscale-0 grayscale"src="02-ja0MTAkK.svg"></img>
-    <img class="h-10 transition duration-300 ease-in-out hover:grayscale-0 grayscale"src="03-ijqoOgvV.svg"></img>
-     <img  class="h-10 transition duration-300 ease-in-out hover:grayscale-0 grayscale"src="04-eZzuwS87.svg"></img>
-    <img  class="h-10 transition duration-300 ease-in-out hover:grayscale-0 grayscale"src="05-t3Mz4baO.svg"></img>
-   <img class="h-10 transition duration-300 ease-in-out hover:grayscale-0 grayscale"src="06-Flg8Aftu.svg"></img>
+        <div className="flex p-20 gap-20">
+    <img className="h-10 transition duration-300 ease-in-out hover:grayscale-0 grayscale" src="download (1).svg"></img>
+    
+    <img className="h-10 transition duration-300 ease-in-out hover:grayscale-0 grayscale"src="02-ja0MTAkK.svg"></img>
+    <img className="h-10 transition duration-300 ease-in-out hover:grayscale-0 grayscale"src="03-ijqoOgvV.svg"></img>
+     <img  className="h-10 transition duration-300 ease-in-out hover:grayscale-0 grayscale"src="04-eZzuwS87.svg"></img>
+    <img  className="h-10 transition duration-300 ease-in-out hover:grayscale-0 grayscale"src="05-t3Mz4baO.svg"></img>
+   <img className="h-10 transition duration-300 ease-in-out hover:grayscale-0 grayscale"src="06-Flg8Aftu.svg"></img>
 </div> 
-<div class="flex px-40 py-10 ">
-  <img class="rounded-xl w-100" src="01-A46SX7OM.jpg"></img>
-  <img class="absolute w-12 left-35" src="download (2).svg"></img>
-  <div class="flex flex-col px-20 py-15 gap-2">
-  <p class=" text-2xl">Moonlight newspaper up its enjoyment agreeable depending. Timed voice share led him to widen noisy young. At weddings believed in laughing</p>
-  <ul class="flex">
+<div className="flex px-40 py-10 ">
+  <img className="rounded-xl w-100" src="01-A46SX7OM.jpg"></img>
+  <img className="absolute w-12 left-35" src="download (2).svg"></img>
+  <div className="flex flex-col px-20 py-15 gap-2">
+  <p className=" text-2xl">Moonlight newspaper up its enjoyment agreeable depending. Timed voice share led him to widen noisy young. At weddings believed in laughing</p>
+  <ul className="flex">
     <li><ImStarFull color='yellow' size="15"/></li>
     <li><ImStarFull color='yellow' size="15"/></li>
     <li><ImStarFull color='yellow' size="15"/></li>
     <li><ImStarFull color='yellow' size="15"/></li>
     <li><FaStarHalfAlt color='yellow' size="15"/></li>
   </ul>
-  <h5 class="font-bold">Billy Vasquez</h5>
-  <span class="opacity-75">Ceo of Apple</span>
+  <h5 className="font-bold">Billy Vasquez</h5>
+  <span className="opacity-75">Ceo of Apple</span>
   </div>
   </div> 
   </div>  
