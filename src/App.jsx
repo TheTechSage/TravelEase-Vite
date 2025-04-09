764f67ceba6124713f897d415127cf02bb48af78
priya <priyaroy.sanoj@gmail.com>
2025-04-09 17:59:27 +0530

    add components in Takeoff

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Header from './components/common/Header'
 import Footer from './components/common/Footer'
 import { Outlet } from 'react-router-dom'
+import Takeoff from './components/flights/home/Takeoff'
 
 function App() {
   return (
     <> 
       <Header />
       <Outlet />
+      <Takeoff />
       <Footer />
     </>
   )
