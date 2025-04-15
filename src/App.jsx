f5d4033969b3cc12ba864570b6eb236ba925b06d
priti <pritiroy.manoj@gmail.com>
2025-04-15 18:20:00 +0530

    added indentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import './App.css'
-import Header from './components/common/Header'
-import Footer from './components/common/Footer'
-import { Outlet } from 'react-router-dom'
+import "./App.css";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
+import { Outlet } from "react-router-dom";
 // import Resort from './components/hotels/Resort'
-import Grid from './components/hotels/Grid'
-import Hero from './components/Hero'
-import Takeoff from './components/flights/home/Takeoff'
+import Grid from "./components/hotels/Grid";
+import Hero from "./components/Hero";
+import Takeoff from "./components/flights/home/Takeoff";
 
 function App() {
   return (
-    <> 
+    <>
       <Header />
       <Outlet />
       {/* <Resort /> */}
-         <Grid />   
-       <Hero />   
+      <Grid />
+      <Hero />
       <Takeoff />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
