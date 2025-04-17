1787c3c4e53b5eb9519e7325ef068ed67de18457
tahirsaifi09 <tahirsaifi413@gmail.com>
2025-04-17 19:17:42 +0530

    Creating Search cab page

---
+++ nextjs/travel/src/app/cabs/page.jsx
@@ -0,0 +1,11 @@
+import Vahicals from "../../components/cabs-home/vahical";
+import SearchCab from "../../components/cabs-home/SearchCab";
+
+export default function CabHome(){
+    return(
+        <div>
+            <SearchCab />
+            <Vahicals />
+        </div>
+    )
+}
---
+++ nextjs/travel/src/components/cabs-home/vahical.jsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+
+export default function Vahicals() {
+  const data = [
+    {
+      icon: "/vahicals-icon/seadan.svg",
+      title: "Sedan",
+      seat: "(6 Sedan)",
+    },
+    {
+      icon: "/vahicals-icon/micro.svg",
+      title: "Micro",
+      seat: "(8 Micros)",
+    },
+    {
+      icon: "/vahicals-icon/suv.svg",
+      title: "SUV",
+      seat: "(6 SUV)",
+    },
+    {
+      icon: "/vahicals-icon/cuv.svg",
+      title: "CUV",
+      seat: "(4 CUVs)",
+    },
+    {
+      icon: "/vahicals-icon/pickup.svg",
+      title: "Pick UP",
+      seat: "(3 Pick ups)",
+    },
+    {
+      icon: "/vahicals-icon/coupe.svg",
+      title: "Coupe",
+      seat: "(9 Coupes)",
+    },
+  ];
+  return (
+    <div>
+      <p className="text-5xl font-bold text-center py-4">
+        Our Awesome Vehicles
+      </p>
+      <div className="flex gap-4 justify-center py-4">
+        {data.map((item, index) => (
+          <div key={index} className="h-60 w-40 flex flex-col gap-2 items-center bg-gray-900 text-white p-4 rounded-xl">
+            <Image src={item.icon} alt="vahical" width={400} height={400} className="bg-gray-700 rounded-full p-4 h-32"/>
+            <p className="text-2xl font-bold">{item.title}</p>
+            <p className="text-gray-400">{item.seat}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
