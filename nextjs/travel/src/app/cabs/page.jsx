5253f3eea0a5677d539dfbc87ae45ec0ff6f4c60
tahirsaifi09 <tahirsaifi413@gmail.com>
2025-04-18 12:53:36 +0530

    Added Choose us component in Cab Home page

--- nextjs/travel/src/app/cabs/page.jsx
+++ nextjs/travel/src/app/cabs/page.jsx
@@ -1,11 +1,13 @@
 import Vahicals from "../../components/cabs-home/vahical";
 import SearchCab from "../../components/cabs-home/SearchCab";
+import ChooseUs from "../../components/cabs-home/choose";
 
 export default function CabHome(){
     return(
-        <div>
+        <div className="">
             <SearchCab />
             <Vahicals />
+            <ChooseUs />
         </div>
     )
 }
